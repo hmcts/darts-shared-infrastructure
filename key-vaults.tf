03040be5e44ab73c14ddd985af25c317ef7e8edb
Dave Thompson <davethompson21@gmail.com>
2023-05-03 15:54:13 +0100

    Fixing key-vault AD group
    - to allow CLI access for creating secrets

--- key-vaults.tf
+++ key-vaults.tf
@@ -8,7 +8,7 @@ module "darts_key_vault" {
   env                     = var.env
   object_id               = var.jenkins_AAD_objectId
   resource_group_name     = azurerm_resource_group.darts_resource_group.name
-  product_group_name      = "DTS Platform Operations"
+  product_group_name      = "DTS Darts Modernisation"
   create_managed_identity = true
 
   common_tags = var.common_tags
