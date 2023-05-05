d77d1ffe7041f5cb5d537fb2a3b737b6a8baff6e
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-05 17:29:47 +0100

    Update blob-storage.tf

--- blob-storage.tf
+++ blob-storage.tf
@@ -17,7 +17,7 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 module "darts" {
-  source                = "https://github.com/hmcts/cnp-module-storage-account/blob/master/containers.tf"
+  source                = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   name                  = "darts"
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "container"
