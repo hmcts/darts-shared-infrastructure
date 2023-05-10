2fe2d3a1e0c2e256fde60020033e44fd65d9e1dd
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-10 11:01:56 +0100

    container variables

--- blob-storage.tf
+++ blob-storage.tf
@@ -10,12 +10,10 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 resource "azurerm_storage_container" "darts" {
-  source                = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
   name = var.storage_container_name
-  resource_group_name   = azurerm_resource_group.darts_resource_group.name
   account_kind          = "StorageV2"
-  env                   = "${var.env}"
   storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
 }
 
 resource "azurerm_storage_blob" "outbound" {
