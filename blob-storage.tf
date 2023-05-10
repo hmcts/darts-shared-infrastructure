c5487c7454213bad6242ed6bcdbeba96b0e33604
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-10 11:34:01 +0100

    Update blob-storage.tf

--- blob-storage.tf
+++ blob-storage.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_account" {
-  name                     =  replace("${var.product}${var.env}", "-", "")
+  storage_account_name     =  replace("${var.product}${var.env}", "-", "")
   resource_group_name      = azurerm_resource_group.darts_resource_group.name
   location                 = "UK South"
   account_tier             = "Standard"
@@ -9,22 +9,27 @@ resource "azurerm_storage_account" "storage_account" {
   tags = var.common_tags
 }
 
-resource "azurerm_storage_container" "darts" {
-  name = var.storage_container_name
+module "darts" {
+  source                = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  name                  = var.storage_container_name
+  storage_container_name = var.storage_container_name
+  resource_group_name   = azurerm_resource_group.darts_resource_group.name
   account_kind          = "StorageV2"
-  storage_account_name  = azurerm_storage_account.storage_account.name
+  env                   = "${var.env}"
+  storage_account_name  = azurerm_storage_account.storage_account.storage_account_name
   container_access_type = "private"
 }
+
 
 resource "azurerm_storage_blob" "outbound" {
   name                   = "${var.product}-outbound-blob-st-${var.env}"
   storage_account_name   = azurerm_storage_account.storage_account.name
-  storage_container_name = azurerm_storage_container.darts.name
+  storage_container_name = module.darts.name
   type                   = "Block"
 }
 resource "azurerm_storage_blob" "unstructured" {
   name                   = "${var.product}-unstrcutured-blob-st-${var.env}"
   storage_account_name   = azurerm_storage_account.storage_account.name
-  storage_container_name = azurerm_storage_container.darts.name
+  storage_container_name = module.darts.name
   type                   = "Block"
 }
