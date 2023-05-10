1b522d639451779bd5d93ee48b2125c3239e1f9d
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-10 10:56:08 +0100

    Update blob-storage.tf

--- blob-storage.tf
+++ blob-storage.tf
@@ -9,25 +9,24 @@ resource "azurerm_storage_account" "storage_account" {
   tags = var.common_tags
 }
 
-module "darts" {
+resource "azurerm_storage_container" "darts" {
   source                = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  storage_container_name = var.storage_container_name
+  name = var.storage_container_name
   resource_group_name   = azurerm_resource_group.darts_resource_group.name
   account_kind          = "StorageV2"
   env                   = "${var.env}"
   storage_account_name  = azurerm_storage_account.storage_account.storage_account_name
 }
 
-
 resource "azurerm_storage_blob" "outbound" {
   name                   = "${var.product}-outbound-blob-st-${var.env}"
   storage_account_name   = azurerm_storage_account.storage_account.name
-  storage_container_name = var.storage_container_name
+  storage_container_name = azurerm_storage_container.darts.name
   type                   = "Block"
 }
 resource "azurerm_storage_blob" "unstructured" {
   name                   = "${var.product}-unstrcutured-blob-st-${var.env}"
   storage_account_name   = azurerm_storage_account.storage_account.name
-  storage_container_name = var.storage_container_name
+  storage_container_name = azurerm_storage_container.darts.name
   type                   = "Block"
 }
