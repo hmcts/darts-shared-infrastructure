48f7d368a298c0f9ddb57d8ef780b1d3e087db2c
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-26 14:09:51 +0100

    module tags and new blob inbound container

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -6,12 +6,26 @@ locals {
 data "azurerm_resource_group" "darts_resource_migration_group" {
     name     = format("%s-migration-%s-rg", var.product, var.env)
 }
+module "tags" {
+  source      = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment = var.env
+  product     = var.product
+  builtFrom   = var.builtFrom
+  expiresAfter = "3000-01-01" # never expire
+}
 
 resource "azurerm_virtual_network" "migration" {
   name                = "migration-vnet"
   address_space       = var.ip_range
   location            = azurerm_resource_group.darts_migration_resource_group.location
   resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
+  business_area       = "sds"
+
+  lifecycle {
+    ignore_changes = [
+      address_space,
+    ]
+  }
 }
 
 resource "azurerm_subnet" "migration" {
@@ -19,6 +33,13 @@ resource "azurerm_subnet" "migration" {
   resource_group_name  = azurerm_resource_group.darts_migration_resource_group.name
   virtual_network_name = azurerm_virtual_network.migration.name
   address_prefixes     = var.ip_range
+
+   lifecycle {
+    ignore_changes = [
+      address_prefixes,
+      service_endpoints,
+    ]
+  }
 }
 
 resource "azurerm_network_interface" "migration" {
