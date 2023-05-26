ffa22493ba3e3660bfcd75c01c8bfc9fa93ad358
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-26 10:56:43 +0100

    Update servers.tf

--- servers.tf
+++ servers.tf
@@ -1,8 +1,16 @@
+locals {
+  vault_name = "${var.product}-migration-${var.env}"
+  rg_name    = "${var.product}-migration-${var.env}-rg"
+}
 
 data "azurerm_resource_group" "darts_resource_migration_group" {
     name     = format("%s-migration-%s-rg", var.product, var.env)
 }
 
+data "azurerm_key_vault" "key_vault" {
+  name                = local.vault_name
+  resource_group_name = local.rg_name
+}
 
 resource "azurerm_virtual_network" "migration" {
   name                = "migration-vnet"
