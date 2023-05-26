2922f4317e4d50eb3b65c4e9702c9548ed33f636
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-05-26 11:45:20 +0100

    Update random-password.tf

--- random-password.tf
+++ random-password.tf
@@ -2,29 +2,9 @@
 resource "random_password" "password" {
   length           = 16
   special          = true
+  min_special      = 1 
+  min_numeric      = 1
+  min_lower        = 1
+  min_upper        = 1
   override_special = "!#$%&*()-_=+[]{}<>:?"
-}
-
-resource "random_password" "certPassword" {
-  length           = 32
-  special          = true
-  override_special = "_%*"
-}
-
-resource "random_password" "restPassword" {
-  length           = 32
-  special          = true
-  override_special = "_%*"
-}
-
-resource "random_password" "streamPassword" {
-  length           = 32
-  special          = true
-  override_special = "_%*"
-}
-
-resource "random_password" "splunk_admin_password" {
-  length           = 32
-  special          = true
-  override_special = "_%*"
 }
