6862f6e79344177e72aa85b2abe411de5afedb0d
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-06-01 11:28:59 +0100

    Update virtual-machine.tf

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -73,7 +73,7 @@ resource "azurerm_virtual_machine" "migration" {
   resource_group_name   = azurerm_resource_group.darts_migration_resource_group.name
   network_interface_ids = [azurerm_network_interface.migration.id]
   vm_size               = "Standard_D8_v3"
-
+  tags = var.common_tags
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
