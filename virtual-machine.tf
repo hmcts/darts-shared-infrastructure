a73dd58a72e735bef5f7846681728b4ed214f77b
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-06-26 10:12:13 +0100

    Update virtual-machine.tf

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -42,8 +42,7 @@ data "azurerm_resource_group" "darts_peer_resource_group" {
 
 resource "azurerm_virtual_network" "peerVN" {
   name                = "peer-vnet"
-  address_space       = var.ip_range
-
+  address_space       = var.ip_range_2
   location            = data.azurerm_resource_group.darts_peer_resource_group.location
   resource_group_name = data.azurerm_resource_group.darts_peer_resource_group.name
   tags = var.common_tags
@@ -59,7 +58,7 @@ resource "azurerm_subnet" "peerSubnet" {
 
   resource_group_name  = data.azurerm_resource_group.darts_peer_resource_group.name
   virtual_network_name = azurerm_virtual_network.migration.name
-  address_prefixes     = var.ip_range
+  address_prefixes     = var.ip_range_2
 
    lifecycle {
     ignore_changes = [
