56db7ce0beb96f821040270e8676f2ee57fce5db
Dave Thompson <davethompson21@gmail.com>
2023-06-26 14:55:53 +0100

    Adding redis
    - for use with the DARTS portal as a session store

---
+++ redis.tf
@@ -0,0 +1,19 @@
+module "darts_redis" {
+  source                   = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product                  = var.product
+  location                 = var.location
+  env                      = var.env
+  common_tags              = var.common_tags
+  redis_version            = "6"
+  business_area            = "sds"
+
+  private_endpoint_enabled      = true
+  public_network_access_enabled = false
+}
+
+resource "azurerm_key_vault_secret" "redis_connection_string" {
+  name         = "redis-connection-string"
+  value        = "redis://ignore:${urlencode(module.darts_redis.access_key)}@${module.darts_redis.host_name}:${module.darts_redis.redis_port}?tls=true"
+
+  key_vault_id = module.darts_key_vault.key_vault_id
+}
