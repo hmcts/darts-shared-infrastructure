2e732de78c0660ffba21d1cba00d5f0db957a905
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-06-28 09:48:07 +0100

    DMP-488-VNET-IP-Range (#45)
    * adding in new ip address for prod usage to stop clash
    
    * Update virtual-machine.tf

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -41,7 +41,7 @@ data "azurerm_resource_group" "darts_resource_migration_group" {
 
 resource "azurerm_virtual_network" "migration" {
   name                = "migration-vnet"
-  address_space       = var.ip_range
+  address_space       = var.env == "prod" ? var.ip_range : var.ip_range_2
   location            = azurerm_resource_group.darts_migration_resource_group.location
   resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
   tags = var.common_tags
