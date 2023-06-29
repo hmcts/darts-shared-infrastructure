938fa2d3ef45c450815e8c739dd5e76af1fde0ab
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-06-29 11:50:36 +0100

    re adding all the vm config

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -39,146 +39,146 @@ data "azurerm_resource_group" "darts_resource_migration_group" {
 }
 
 
-# resource "azurerm_virtual_network" "migration" {
-#   name                = "migration-vnet"
-#   address_space       =  [var.address_space]
-#   location            = azurerm_resource_group.darts_migration_resource_group.location
-#   resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
-#   tags = var.common_tags
-#   lifecycle {
-#     ignore_changes = [
-#       address_space,
-#     ]
-#   }
-# }
+resource "azurerm_virtual_network" "migration" {
+  name                = "migration-vnet"
+  address_space       =  [var.address_space]
+  location            = azurerm_resource_group.darts_migration_resource_group.location
+  resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
+  tags = var.common_tags
+  lifecycle {
+    ignore_changes = [
+      address_space,
+    ]
+  }
+}
 
-# resource "azurerm_subnet" "migration" {
-#   name                 = "migration-subnet"
-#   resource_group_name  = azurerm_resource_group.darts_migration_resource_group.name
-#   virtual_network_name = azurerm_virtual_network.migration.name
-#   address_prefixes     = [var.address_space]
+resource "azurerm_subnet" "migration" {
+  name                 = "migration-subnet"
+  resource_group_name  = azurerm_resource_group.darts_migration_resource_group.name
+  virtual_network_name = azurerm_virtual_network.migration.name
+  address_prefixes     = [var.address_space]
 
-#    lifecycle {
-#     ignore_changes = [
-#       address_prefixes,
-#       service_endpoints,
-#     ]
-#   }
-# }
+   lifecycle {
+    ignore_changes = [
+      address_prefixes,
+      service_endpoints,
+    ]
+  }
+}
 
-# data "azurerm_virtual_network" "hub-south-vnet" {
-#   provider            = azurerm.hub
-#   name                = local.hub[var.hub].ukSouth.name
-#   resource_group_name = local.hub[var.hub].ukSouth.name
-# }
-
-
-# resource "azurerm_virtual_network_peering" "migration_to_hub" {
-#   name                 = "migration-to-hub"
-#   resource_group_name  = azurerm_resource_group.darts_migration_resource_group.name
-#   virtual_network_name = azurerm_virtual_network.migration.name
-#   remote_virtual_network_id = data.azurerm_virtual_network.hub-south-vnet.id
-#   allow_virtual_network_access = true
-#   allow_forwarded_traffic = true
-#   allow_gateway_transit = false
-# }
-
-# resource "azurerm_virtual_network_peering" "hub_to_migration" {
-#   provider             = azurerm.hub
-#   name                 = "hub-to-migration"
-#   resource_group_name  = local.hub[var.hub].ukSouth.name
-#   virtual_network_name = local.hub[var.hub].ukSouth.name
-#   remote_virtual_network_id = azurerm_virtual_network.migration.id
-#   allow_virtual_network_access = true
-#   allow_forwarded_traffic = true
-#   allow_gateway_transit = false
-# }
-
-# resource "azurerm_route_table" "peering" {
-#   name                = "vnetToPauloAlto"
-#   resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
-#   location            = azurerm_resource_group.darts_migration_resource_group.location
-#   tags                = var.common_tags
-# }
-
-# resource "azurerm_route" "route" {
-#   name                = "DefaultRoute"
-#   resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
-#   route_table_name    = azurerm_route_table.peering.name
-#   address_prefix      = var.env == "prod" ? var.paloaltoProd : var.paloaltoNonProd 
-#   next_hop_type       = "VirtualNetworkGateway"
-# }
+data "azurerm_virtual_network" "hub-south-vnet" {
+  provider            = azurerm.hub
+  name                = local.hub[var.hub].ukSouth.name
+  resource_group_name = local.hub[var.hub].ukSouth.name
+}
 
 
+resource "azurerm_virtual_network_peering" "migration_to_hub" {
+  name                 = "migration-to-hub"
+  resource_group_name  = azurerm_resource_group.darts_migration_resource_group.name
+  virtual_network_name = azurerm_virtual_network.migration.name
+  remote_virtual_network_id = data.azurerm_virtual_network.hub-south-vnet.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic = true
+  allow_gateway_transit = false
+}
+
+resource "azurerm_virtual_network_peering" "hub_to_migration" {
+  provider             = azurerm.hub
+  name                 = "hub-to-migration"
+  resource_group_name  = local.hub[var.hub].ukSouth.name
+  virtual_network_name = local.hub[var.hub].ukSouth.name
+  remote_virtual_network_id = azurerm_virtual_network.migration.id
+  allow_virtual_network_access = true
+  allow_forwarded_traffic = true
+  allow_gateway_transit = false
+}
+
+resource "azurerm_route_table" "peering" {
+  name                = "vnetToPauloAlto"
+  resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
+  location            = azurerm_resource_group.darts_migration_resource_group.location
+  tags                = var.common_tags
+}
+
+resource "azurerm_route" "route" {
+  name                = "DefaultRoute"
+  resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
+  route_table_name    = azurerm_route_table.peering.name
+  address_prefix      = var.env == "prod" ? var.paloaltoProd : var.paloaltoNonProd 
+  next_hop_type       = "VirtualNetworkGateway"
+}
 
 
-# resource "azurerm_network_interface" "migration" {
-#   name                = "migration-nic"
-#   location            = azurerm_resource_group.darts_migration_resource_group.location
-#   resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
-#   tags = var.common_tags
 
-#   ip_configuration {
-#     name                          = "migration-ipconfig"
-#     subnet_id                     = azurerm_subnet.migration.id
-#     private_ip_address_allocation = "Dynamic"
-#   }
-# }
 
-# resource "azurerm_managed_disk" "migration_os" {
-#   name                 = "migration-osdisk"
-#   location             = azurerm_resource_group.darts_migration_resource_group.location
-#   resource_group_name  = azurerm_resource_group.darts_migration_resource_group.name
-#   storage_account_type = "Standard_LRS"
-#   create_option        = "Empty"
-#   disk_size_gb         = "20"
-#   tags = var.common_tags
-# }
+resource "azurerm_network_interface" "migration" {
+  name                = "migration-nic"
+  location            = azurerm_resource_group.darts_migration_resource_group.location
+  resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
+  tags = var.common_tags
 
-# resource "azurerm_managed_disk" "migration_data" {
-#   name                 = "migration-datadisk"
-#   location             = azurerm_resource_group.darts_migration_resource_group.location
-#   resource_group_name  = azurerm_resource_group.darts_migration_resource_group.name
-#   storage_account_type = "Standard_LRS"
-#   create_option        = "Empty"
-#   disk_size_gb         = "200"
-#   tags = var.common_tags
-# }
+  ip_configuration {
+    name                          = "migration-ipconfig"
+    subnet_id                     = azurerm_subnet.migration.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
-# resource "azurerm_linux_virtual_machine" "migration" {
-#   name                  = "migration-vm"
-#   location              = azurerm_resource_group.darts_migration_resource_group.location
-#   resource_group_name   = azurerm_resource_group.darts_migration_resource_group.name
-#   network_interface_ids = [azurerm_network_interface.migration.id]
-#   size                  = "Standard_D8ds_v5"
-#   tags                  = var.common_tags
-#   admin_username        = var.admin_user
-#   admin_password        = random_password.password.result
-#   disable_password_authentication = false
+resource "azurerm_managed_disk" "migration_os" {
+  name                 = "migration-osdisk"
+  location             = azurerm_resource_group.darts_migration_resource_group.location
+  resource_group_name  = azurerm_resource_group.darts_migration_resource_group.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "20"
+  tags = var.common_tags
+}
 
-#   os_disk {
-#     caching           = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
+resource "azurerm_managed_disk" "migration_data" {
+  name                 = "migration-datadisk"
+  location             = azurerm_resource_group.darts_migration_resource_group.location
+  resource_group_name  = azurerm_resource_group.darts_migration_resource_group.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "200"
+  tags = var.common_tags
+}
 
-#   source_image_reference {
-#     publisher = "canonical"
-#     offer     = "0001-com-ubuntu-server-jammy"
-#     sku       = "22_04-lts-gen2"
-#     version   = "latest"
-#   }
-#   identity {
-#     type = "SystemAssigned"
-#   }
+resource "azurerm_linux_virtual_machine" "migration" {
+  name                  = "migration-vm"
+  location              = azurerm_resource_group.darts_migration_resource_group.location
+  resource_group_name   = azurerm_resource_group.darts_migration_resource_group.name
+  network_interface_ids = [azurerm_network_interface.migration.id]
+  size                  = "Standard_D8ds_v5"
+  tags                  = var.common_tags
+  admin_username        = var.admin_user
+  admin_password        = random_password.password.result
+  disable_password_authentication = false
 
-# }
+  os_disk {
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
   
-# resource "azurerm_virtual_machine_data_disk_attachment" "datadisk" {
-#   managed_disk_id    = azurerm_managed_disk.migration_data.id
-#   virtual_machine_id = azurerm_linux_virtual_machine.migration.id
-#   lun                = "10"
-#   caching            = "ReadWrite"
-# }
+resource "azurerm_virtual_machine_data_disk_attachment" "datadisk" {
+  managed_disk_id    = azurerm_managed_disk.migration_data.id
+  virtual_machine_id = azurerm_linux_virtual_machine.migration.id
+  lun                = "10"
+  caching            = "ReadWrite"
+}
 
 
 resource "azurerm_key_vault_secret" "os_profile_password" {
