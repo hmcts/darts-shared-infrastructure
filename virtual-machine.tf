a123fd58b2bd7e98d391457cf5a3e2f4a0887707
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-07-05 08:58:06 +0100

    Update virtual-machine.tf

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -184,7 +184,15 @@ resource "azurerm_linux_virtual_machine" "migration" {
    caching            = "ReadWrite"
  }
 
-
+resource "azurerm_virtual_machine_extension" "bastion_aad" {
+  name                       = "AADSSHLoginForLinux"
+  virtual_machine_id         = azurerm_linux_virtual_machine.migration-vm.id
+  publisher                  = "Microsoft.Azure.ActiveDirectory"
+  type                       = "AADSSHLoginForLinux"
+  type_handler_version       = "1.0"
+  auto_upgrade_minor_version = true
+  tags                       = var.common_tags
+}
 resource "azurerm_key_vault_secret" "os_profile_password" {
   name         = "os-profile-password"
   value        = random_password.password.result
