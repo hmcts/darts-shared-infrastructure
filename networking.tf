1b68b31eaae9e2c6688047a0c469d7a19b4abf0a
Alex Bance <Alex.Bance@justice.gov.uk>
2023-07-27 20:44:50 +0100

    fix: swap key value

--- networking.tf
+++ networking.tf
@@ -65,9 +65,9 @@ resource "azurerm_route" "route" {
 }
 
 resource "azurerm_route" "azure_firewall_routes" {
-  for_each = { for key, value in var.az_firewall_route_ranges : value => key }
+  for_each = { for key, value in var.az_firewall_route_ranges : key => value }
 
-  name                   = "azure_firewall_routes_${replace(split("/", each.key)[0], ".", "_")}"
+  name                   = "azure_firewall_routes_${replace(split("/", each.value)[0], ".", "_")}"
   resource_group_name    = azurerm_resource_group.darts_migration_resource_group.name
   route_table_name       = azurerm_route_table.route_table.name
   address_prefix         = each.value
