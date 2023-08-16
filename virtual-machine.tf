d833fd63df2e5e80485b83fbb15872da7d734876
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-08-16 14:33:33 +0100

    Update virtual-machine.tf

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -124,7 +124,7 @@ resource "azurerm_windows_virtual_machine" "assessment_windows" {
 resource "azurerm_windows_virtual_machine" "migration_windows" {
 
   for_each = toset(var.windows_machines)
-  name                = each.value.windows_machines
+  name                = each.value.windows_machines.default
   location            = azurerm_resource_group.darts_migration_resource_group.location
   resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
   size                = "Standard_D8ds_v5"
@@ -132,14 +132,14 @@ resource "azurerm_windows_virtual_machine" "migration_windows" {
   admin_username      = var.admin_user
   admin_password      = random_password.password.result
   provision_vm_agent  = true
-  computer_name       = each.value.windows_machines
+  computer_name       = each.value.windows_machines.default
   network_interface_ids = [
     azurerm_network_interface.assessment.id,
   ]
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    name                 = "${each.value.windows_machines}-OsDisk"
+    name                 = "${each.value.windows_machines.default}-OsDisk"
   }
 
   source_image_reference {
