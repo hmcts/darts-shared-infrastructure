bba5ee47ea754c14707c6dc246ab4380b96db570
Alex Bance <Alex.Bance@justice.gov.uk>
2023-09-04 12:22:42 +0100

    fix: issue with the attempted creation of multiple MIs

--- key-vaults.tf
+++ key-vaults.tf
@@ -17,13 +17,16 @@ module "darts_key_vault" {
 module "darts_migration_key_vault" {
   source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
 
-  name                    = format("darts-migration-%s", var.env)
-  product                 = var.product
-  env                     = var.env
-  object_id               = var.jenkins_AAD_objectId
-  resource_group_name     = azurerm_resource_group.darts_migration_resource_group.name
-  product_group_name      = "DTS Darts Modernisation"
-  create_managed_identity = true
+  depends_on = [module.darts_key_vault]
+
+  name                        = format("darts-migration-%s", var.env)
+  product                     = var.product
+  env                         = var.env
+  object_id                   = var.jenkins_AAD_objectId
+  resource_group_name         = azurerm_resource_group.darts_migration_resource_group.name
+  product_group_name          = "DTS Darts Modernisation"
+  create_managed_identity     = false
+  managed_identity_object_ids = module.darts_key_vault.managed_identity_objectid
 
   common_tags = var.common_tags
 }
