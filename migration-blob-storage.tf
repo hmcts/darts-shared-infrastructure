6bb70d50398f5eadf0c307030e5a37e4a70f857b
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-10-17 16:48:34 +0100

    Update migration-blob-storage.tf

--- migration-blob-storage.tf
+++ migration-blob-storage.tf
@@ -3,41 +3,20 @@ data "azurerm_resource_group" "darts_migration_resource_group" {
 }
 
 
+
 module "sa-migration" {
-  source = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-
-  env = var.env
-
-  storage_account_name = local.migration_storage_account_name
-  common_tags          = var.common_tags
-
-  default_action = "Allow"
-
-  resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
-  location            = var.location
-
-  account_tier                    = var.sa_account_tier
-  account_kind                    = var.sa_account_kind
-  account_replication_type        = var.sa_account_replication_type
-  access_tier                     = var.sa_access_tier
-  allow_nested_items_to_be_public = "true"
-  enable_change_feed              = true
-
-  enable_data_protection = true
-
+  source                   = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                      = var.env
+  storage_account_name     = local.migration_storage_account_name
+  resource_group_name      = azurerm_resource_group.darts_migration_resource_group.name
+  location                 = var.location
+  account_kind             = var.account_kind
+  account_replication_type = var.account_replication_type
   containers = local.containers
-
-  cors_rules = [{
-    allowed_headers    = ["*"]
-    allowed_methods    = ["GET", "OPTIONS"]
-    allowed_origins    = ["https://hmctsdartsb2csbox.b2clogin.com"]
-    exposed_headers    = ["*"]
-    max_age_in_seconds = 200
-  }]
-
+  private_endpoint_subnet_id = resource.azurerm_subnet.migration.id
 }
 
-resource "azurerm_storage_blob" "migration-unstrcutured" {
+resource "azurerm_storage_blob" "migration-st" {
   name                   = "${var.product}-migration-blob-st-${var.env}"
   storage_account_name   = local.migration_storage_account_name
   storage_container_name = local.darts_migration_container
