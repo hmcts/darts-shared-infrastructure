b98401004fe21f07a0e0d09e485b012b88890eff
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-10-18 14:24:25 +0100

    Update virtual-machine.tf

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -121,10 +121,8 @@ resource "azurerm_windows_virtual_machine" "assessment_windows" {
   }
 }
 
-resource "azurerm_windows_virtual_machine" "migration_windows" {
-
-  for_each = toset(var.windows_machines)
-  name                = each.value
+resource "azurerm_windows_virtual_machine" "assessment_windows" {
+  name                = "assessment-windows"
   location            = azurerm_resource_group.darts_migration_resource_group.location
   resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
   size                = "Standard_D8ds_v5"
@@ -132,14 +130,14 @@ resource "azurerm_windows_virtual_machine" "migration_windows" {
   admin_username      = var.admin_user
   admin_password      = random_password.password.result
   provision_vm_agent  = true
-  computer_name       = each.value
+  computer_name       = "winAssessment"
   network_interface_ids = [
     azurerm_network_interface.assessment.id,
   ]
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    name                 = "${each.value}-OsDisk"
+    name                 = "Windows-Assesment-OsDisk"
   }
 
   source_image_reference {
