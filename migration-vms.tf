7893bc201407067acc85992c375dcd80b933c3b1
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-10-19 15:33:58 +0100

    Update migration-vms.tf

--- migration-vms.tf
+++ migration-vms.tf
@@ -57,3 +57,28 @@ resource "azurerm_virtual_machine_data_disk_attachment" "migration_vms_datadisk"
   lun                = "10"
   caching            = "ReadWrite"
 }
+
+resource "azurerm_linux_virtual_machine" "migration" {
+  name                            = "migration-vm"
+  location                        = azurerm_resource_group.darts_migration_resource_group.location
+  resource_group_name             = azurerm_resource_group.darts_migration_resource_group.name
+  network_interface_ids           = [azurerm_network_interface.migration.id]
+  size                            = "Standard_D8ds_v5"
+  tags                            = var.common_tags
+  admin_username                  = var.admin_user
+  admin_password                  = random_password.password.result
+  disable_password_authentication = false
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Cannonical"
+    offer     = "UbuntuServer"
+    sku       = "22.04.03-LTS"
+    version   = "latest"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
