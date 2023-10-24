c455734633f4364318a41307d0d45ed20b795aae
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-10-24 12:27:37 +0100

    creating a new NIC, and adding the ability to create more linux vm's if needed

--- migration-vms.tf
+++ migration-vms.tf
@@ -58,12 +58,27 @@ resource "azurerm_virtual_machine_data_disk_attachment" "migration_vms_datadisk"
   caching            = "ReadWrite"
 }
 
+resource "azurerm_network_interface" "migration-linux-nic" {
+  for_each            = var.migration_linux_vms
+  name                = "${each.key}-nic"
+  location            = azurerm_resource_group.darts_migration_resource_group.location
+  resource_group_name = azurerm_resource_group.darts_migration_resource_group.name
+  tags                = var.common_tags
+
+  ip_configuration {
+    name                          = "migration-ipconfig"
+    subnet_id                     = azurerm_subnet.migration.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 resource "azurerm_linux_virtual_machine" "migration-linux" {
-  name                            = "${var.env}dartsmigdb01"
+  for_each                        = var.migration_linux_vms
+  name                            = each.key
   location                        = azurerm_resource_group.darts_migration_resource_group.location
   resource_group_name             = azurerm_resource_group.darts_migration_resource_group.name
-  network_interface_ids           = [azurerm_network_interface.migration.id]
-  size                            = var.env == "prd" ? "Standard_E32d_v5" : "Standard_D8ds_v5"
+  network_interface_ids           = [azurerm_network_interface.migration-linux-nic.id]
+  size                            =  "Standard_E32d_v5" 
   tags                            = var.common_tags
   admin_username                  = var.admin_user
   admin_password                  = random_password.password.result
