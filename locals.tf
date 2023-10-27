8ce7fd3236f0445d68a380185a38331d3695e2bb
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-10-27 14:00:51 +0100

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -26,7 +26,7 @@ locals {
     }
   }
   storage_account_name = "${var.product}sa${var.env}"
-  migration_storage_account_name = "${var.product}-mig-${var.env}"
+  migration_storage_account_name = "${var.product}mig${var.env}"
   containers = [{
     name        = "darts-outbound"
     access_type = "private"
