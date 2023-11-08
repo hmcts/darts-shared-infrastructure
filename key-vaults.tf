c6d27b9d699efa36bc656bb58e8471d703f8b9dd
Alex Bance <Alex.Bance@justice.gov.uk>
2023-11-08 15:04:06 +0000

    fix: switch back to master & add additional developer group

--- key-vaults.tf
+++ key-vaults.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 module "darts_key_vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=feat%2Fignore_metric_changes"
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
 
   name                    = format("darts-%s", var.env)
   product                 = var.product
@@ -9,13 +9,14 @@ module "darts_key_vault" {
   object_id               = var.jenkins_AAD_objectId
   resource_group_name     = azurerm_resource_group.darts_resource_group.name
   product_group_name      = "DTS Darts Modernisation"
+  developer_group         = local.admin_group_map[var.env]
   create_managed_identity = true
 
   common_tags = var.common_tags
 }
 
 module "darts_migration_key_vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=feat%2Fignore_metric_changes"
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
 
   depends_on = [module.darts_key_vault]
 
@@ -25,6 +26,7 @@ module "darts_migration_key_vault" {
   object_id                   = var.jenkins_AAD_objectId
   resource_group_name         = azurerm_resource_group.darts_migration_resource_group.name
   product_group_name          = "DTS Darts Modernisation"
+  developer_group             = local.admin_group_map[var.env]
   create_managed_identity     = false
   managed_identity_object_ids = module.darts_key_vault.managed_identity_objectid
 
