758f72cedf2388d005f9dfecd8103c4048ed9db3
Alex Bance <Alex.Bance@justice.gov.uk>
2023-11-08 14:51:13 +0000

    feat: switch branch to test

--- key-vaults.tf
+++ key-vaults.tf
@@ -1,21 +1,21 @@
 data "azurerm_client_config" "current" {}
 
 module "darts_key_vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=feat%2Fignore_metric_changes"
 
   name                    = format("darts-%s", var.env)
   product                 = var.product
   env                     = var.env
   object_id               = var.jenkins_AAD_objectId
   resource_group_name     = azurerm_resource_group.darts_resource_group.name
-  developers_group        = "DTS Darts Modernisation"
+  product_group_name      = "DTS Darts Modernisation"
   create_managed_identity = true
 
   common_tags = var.common_tags
 }
 
 module "darts_migration_key_vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=feat%2Fignore_metric_changes"
 
   depends_on = [module.darts_key_vault]
 
@@ -24,7 +24,7 @@ module "darts_migration_key_vault" {
   env                         = var.env
   object_id                   = var.jenkins_AAD_objectId
   resource_group_name         = azurerm_resource_group.darts_migration_resource_group.name
-  developers_group            = "DTS Darts Modernisation"
+  product_group_name          = "DTS Darts Modernisation"
   create_managed_identity     = false
   managed_identity_object_ids = module.darts_key_vault.managed_identity_objectid
 
