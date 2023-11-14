bdf319bb7722ccc96fd47aad6ee1a180a1ad56e3
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-11-14 13:50:01 +0000

    Update database.tf

--- database.tf
+++ database.tf
@@ -66,10 +66,10 @@ module "postgresql_flexible" {
   location            = var.location
   pgsql_storage_mb    = 4194304
   pgsql_sku           = "GP_Standard_D8ds_v4"
-  
 
-  common_tags                        = var.common_tags
-  admin_user_object_id           = var.jenkins_AAD_objectId
+
+  common_tags               = var.common_tags
+  admin_user_object_id      = var.jenkins_AAD_objectId
   pgsql_delegated_subnet_id = azurerm_subnet.postgres.id
   pgsql_databases = [
     {
