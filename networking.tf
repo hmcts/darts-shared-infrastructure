a0fd467183f2456e9e9355bdd3a77e4f8682e361
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-12-06 14:32:21 +0000

    fixing vnet

--- networking.tf
+++ networking.tf
@@ -11,7 +11,7 @@ resource "azurerm_subnet" "migration" {
   for_each = contains(["stg", "prod"], var.env) ? var.create_resource : {}
   name                 = "migration-subnet"
   resource_group_name  = azurerm_resource_group.darts_migration_resource_group[each.key].name
-  virtual_network_name = azurerm_virtual_network.migration.name
+  virtual_network_name = azurerm_virtual_network.migration[each.key].name
   address_prefixes     = [var.address_space]
 }
 
@@ -25,7 +25,7 @@ resource "azurerm_virtual_network_peering" "darts_migration_to_hub" {
   for_each = contains(["stg", "prod"], var.env) ? var.create_resource : {}
   name                         = "darts-migration-to-hub-${var.env}"
   resource_group_name          = azurerm_resource_group.darts_migration_resource_group[each.key].name
-  virtual_network_name         = azurerm_virtual_network.migration.name
+  virtual_network_name         = azurerm_virtual_network.migration[each.key].name
   remote_virtual_network_id    = data.azurerm_virtual_network.hub-south-vnet.id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
@@ -38,7 +38,7 @@ resource "azurerm_virtual_network_peering" "hub_to_darts_migration" {
   name                         = "hub-to-darts-migration-${var.env}"
   resource_group_name          = local.hub[var.hub].ukSouth.name
   virtual_network_name         = local.hub[var.hub].ukSouth.name
-  remote_virtual_network_id    = azurerm_virtual_network.migration.id
+  remote_virtual_network_id    = azurerm_virtual_network.migration[each.key].id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
   allow_gateway_transit        = false
@@ -56,7 +56,7 @@ resource "azurerm_route" "route" {
   for_each = contains(["stg", "prod"], var.env) ? var.create_resource : {}
   name                   = "DefaultRoute"
   resource_group_name    = azurerm_resource_group.darts_migration_resource_group[each.key].name
-  route_table_name       = azurerm_route_table.route_table.name
+  route_table_name       = azurerm_route_table.route_table[each.key].name
   address_prefix         = "0.0.0.0/0"
   next_hop_type          = "VirtualAppliance"
   next_hop_in_ip_address = local.hub[var.hub].ukSouth.next_hop_ip
