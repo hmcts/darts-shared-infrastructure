0b0a3b547ce468faeccd71207b2af76a18c30ea6
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-12-06 17:51:29 +0000

    Update database.tf
    Co-authored-by: Alex Bance <abance@bancey.xyz>
    Signed-off-by: scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>

--- database.tf
+++ database.tf
@@ -27,7 +27,7 @@ resource "azurerm_key_vault_secret" "POSTGRES_USER" {
 resource "azurerm_key_vault_secret" "POSTGRES_PASS" {
   for_each = contains(["stg", "prod"], var.env) ? var.create_resource : {}
   name         = "POSTGRES-PASS"
-  value        = module.postgresql_flexible.password
+  value        = module.postgresql_flexible[0].password
   key_vault_id = module.darts_migration_key_vault[each.key].key_vault_id
   depends_on   = [module.darts_migration_key_vault]
 }
