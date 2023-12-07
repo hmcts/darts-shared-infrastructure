6d55c3e381b378726a95647b94b44c72a70ef4e5
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-12-07 13:19:28 +0000

    Update role-assignment.tf

--- role-assignment.tf
+++ role-assignment.tf
@@ -1,8 +1,8 @@
 resource "azurerm_role_assignment" "vm-admin" {
-  count = contains(["stg", "prod"], var.env) ? var.virtual_machine_admins : {}
-  scope                = azurerm_linux_virtual_machine.migration[0].id
+  for_each = contains(["stg", "prod"], var.env) ? var.virtual_machine_admins : {}
+  scope                = azurerm_linux_virtual_machine.migration[each.key].id
   role_definition_name = "Virtual Machine Administrator Login"
-  principal_id         = var.virtual_machine_admins[0]
+  principal_id         = var.virtual_machine_admins[each.key]
 }
 
 # resource "azurerm_role_assignment" "vm-user" {
@@ -10,4 +10,4 @@ resource "azurerm_role_assignment" "vm-admin" {
 #   scope                = azurerm_linux_virtual_machine.migration[each.key].id
 #   role_definition_name = "Virtual Machine User Login"
 #   principal_id         = var.virtual_machine_users[each.key]
-# }count = contains(["stg", "prod"], var.env) ? 1 : 0
+# }
