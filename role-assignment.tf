07d01dd3efe50885ed6b7619954cf29dc2b0504d
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2023-12-07 15:09:26 +0000

    Update role-assignment.tf

--- role-assignment.tf
+++ role-assignment.tf
@@ -1,7 +1,7 @@
 resource "azurerm_role_assignment" "vm-admin" {
   count = contains(["stg", "prod"], var.env) ? var.virtual_machine_admins : {}
   # for_each = contains(["stg", "prod"], var.env) ? var.virtual_machine_admins : {}
-  scope                = azurerm_linux_virtual_machine.migration[each.key].id
+  scope                = azurerm_linux_virtual_machine.migration[count.index].id
   role_definition_name = "Virtual Machine Administrator Login"
   principal_id         = var.virtual_machine_admins[count.index]
 }
