49e59427711bd9591e4678448275a3c3a3982b31
Richard.Penswick <Richard.Penswick@cgi.com>
2023-12-07 16:04:29 +0000

    resolve for each var issue

--- role-assignment.tf
+++ role-assignment.tf
@@ -1,6 +1,6 @@
 resource "azurerm_role_assignment" "vm-admin" {
-  for_each = contains(["stg", "prod"], var.env) ? var.virtual_machine_admins : {}
-  scope                = azurerm_linux_virtual_machine.migration[each.key[1]].id
+  for_each             = contains(["stg", "prod"], var.env) ? var.virtual_machine_admins : {}
+  scope                = azurerm_linux_virtual_machine.migration[each.key].id
   role_definition_name = "Virtual Machine Administrator Login"
   principal_id         = var.virtual_machine_admins[each.key]
 }
