20f0c4f32677e7f8fbcfbbbe4f15f25fa1bb8e2a
Richard.Penswick <Richard.Penswick@cgi.com>
2023-12-07 16:54:24 +0000

    change for each logic

--- role-assignment.tf
+++ role-assignment.tf
@@ -1,5 +1,5 @@
 resource "azurerm_role_assignment" "vm-admin" {
-  for_each             = contains(["stg", "prod"], var.env) ? var.virtual_machine_admins : false
+  for_each             = contains(["stg", "prod"], var.env) ? var.virtual_machine_admins : []
   scope                = azurerm_linux_virtual_machine.migration[each.key].id
   role_definition_name = "Virtual Machine Administrator Login"
   principal_id         = var.virtual_machine_admins[each.key]
