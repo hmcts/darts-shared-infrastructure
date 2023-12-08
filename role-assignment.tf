33615c3fd8870aa89fbf84ac3d3c1ef723ff986e
Richard.Penswick <Richard.Penswick@cgi.com>
2023-12-08 10:16:15 +0000

    fix for each loop logic

--- role-assignment.tf
+++ role-assignment.tf
@@ -1,8 +1,8 @@
 resource "azurerm_role_assignment" "vm-admin" {
   for_each             = contains(["stg", "prod"], var.env) ? var.virtual_machine_admins : {}
-  scope                = azurerm_linux_virtual_machine.migration[each.key].id
+  scope                = azurerm_linux_virtual_machine.migration[each.value].id
   role_definition_name = "Virtual Machine Administrator Login"
-  principal_id         = var.virtual_machine_admins[each.key]
+  principal_id         = var.virtual_machine_admins[each.value]
 }
 
 # resource "azurerm_role_assignment" "vm-user" {
