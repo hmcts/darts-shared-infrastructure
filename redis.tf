678dff5cffcd1bb2958d0181ed9be09fcf0a4fb1
Alex Bance <Alex.Bance@justice.gov.uk>
2023-12-13 12:43:27 +0000

    fix: update redis config & move to existing rg

--- redis.tf
+++ redis.tf
@@ -1,8 +1,7 @@
 module "darts_redis" {
-  count                         = local.is_migration_environment ? 1 : 0
   source                        = "git@github.com:hmcts/cnp-module-redis?ref=master"
   product                       = var.product
-  location                      = var.location
+  location                      = azurerm_resource_group.darts_resource_group.location
   env                           = var.env
   common_tags                   = var.common_tags
   redis_version                 = "6"
@@ -12,22 +11,21 @@ module "darts_redis" {
   sku_name                      = var.sku_name
   family                        = var.family
   capacity                      = var.capacity
+  resource_group_name           = azurerm_resource_group.darts_resource_group.name
 }
 
 moved {
-  from = module.darts_redis
-  to   = module.darts_redis[0]
+  from = module.darts_redis[0]
+  to   = module.darts_redis
 }
 
 resource "azurerm_key_vault_secret" "redis_connection_string" {
-  count = local.is_migration_environment ? 1 : 0
-  name  = "redis-connection-string"
-  value = "rediss://:${urlencode(module.darts_redis[0].access_key)}@${module.darts_redis[0].host_name}:${module.darts_redis[0].redis_port}?tls=true"
-
+  name         = "redis-connection-string"
+  value        = "rediss://:${urlencode(module.darts_redis.access_key)}@${module.darts_redis.host_name}:${module.darts_redis.redis_port}?tls=true"
   key_vault_id = module.darts_key_vault.key_vault_id
 }
 
 moved {
-  from = azurerm_key_vault_secret.redis_connection_string
-  to   = azurerm_key_vault_secret.redis_connection_string[0]
+  from = azurerm_key_vault_secret.redis_connection_string[0]
+  to   = azurerm_key_vault_secret.redis_connection_string
 }
