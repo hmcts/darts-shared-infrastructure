59e7d9de18c409ba8233951b4344be208bb0515f
Alex Bance <Alex.Bance@justice.gov.uk>
2023-12-14 09:42:35 +0000

    tmp: test plan without linking KeyVault MIs

--- key-vaults.tf
+++ key-vaults.tf
@@ -3,7 +3,7 @@ data "azurerm_client_config" "current" {}
 module "darts_key_vault" {
   source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
 
-  name                    = format("darts-%s", var.env)
+  name                    = "darts-${var.env}"
   product                 = var.product
   env                     = var.env
   object_id               = var.jenkins_AAD_objectId
@@ -21,7 +21,7 @@ module "darts_migration_key_vault" {
 
   depends_on = [module.darts_key_vault]
 
-  name                        = format("darts-migration-%s", var.env)
+  name                        = "darts-migration-${var.env}"
   product                     = var.product
   env                         = var.env
   object_id                   = var.jenkins_AAD_objectId
@@ -29,7 +29,7 @@ module "darts_migration_key_vault" {
   product_group_name          = "DTS Darts Modernisation"
   developers_group            = local.admin_group_map[var.env]
   create_managed_identity     = false
-  managed_identity_object_ids = module.darts_key_vault.managed_identity_objectid
+  #managed_identity_object_ids = module.darts_key_vault.managed_identity_objectid
 
   common_tags = var.common_tags
 }
