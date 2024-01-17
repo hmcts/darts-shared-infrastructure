cdbd41551b35bb950691858b083a27f8eaf3ba16
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-01-17 11:10:06 +0000

    Ddm-88-oracle-vm (#138)
    * new oracle machine
    
    * Update migration-vms.tf
    
    * Update prod.tfvars
    
    * Update migration-vms.tf
    
    * Update migration-vms.tf
    
    * Update migration-vms.tf
    
    * Update migration-vms.tf
    
    * removing old migration storage account

--- migration-blob-storage.tf
+++ migration-blob-storage.tf
@@ -1,23 +1,3 @@
-module "sa-migration" {
-  count                             = local.is_migration_environment ? 1 : 0
-  source                            = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  env                               = var.env
-  storage_account_name              = local.migration_storage_account_name
-  resource_group_name               = azurerm_resource_group.darts_migration_resource_group[0].name
-  location                          = var.location
-  account_kind                      = var.sa_mig_account_kind
-  account_tier                      = var.sa_mig_account_tier
-  account_replication_type          = var.sa_mig_account_replication_type
-  containers                        = local.containers-mig
-  private_endpoint_subnet_id        = resource.azurerm_subnet.migration[0].id
-  enable_nfs                        = true
-  enable_hns                        = true
-  enable_data_protection            = true
-  enable_versioning                 = false
-  defender_enabled                  = var.defender_enable
-  defender_malware_scanning_enabled = var.defender_scan
-  common_tags                       = var.common_tags
-}
 
 module "sa-migration-standard" {
   count                             = local.is_migration_environment ? 1 : 0
