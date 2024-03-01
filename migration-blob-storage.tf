5164ab205b9fdf70d59460c3631f3c2ce42698f0
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-03-01 10:51:42 +0000

    Update migration-blob-storage.tf

--- migration-blob-storage.tf
+++ migration-blob-storage.tf
@@ -17,7 +17,7 @@ module "sa-migration-standard" {
   enable_versioning                 = false
   defender_enabled                  = var.defender_enable
   defender_malware_scanning_enabled = var.defender_scan
-  defender_malware_scanning_cap_gb_per_month = 10000
+  defender_malware_scanning_cap_gb_per_month = 150000
   common_tags                       = var.common_tags
 }
 
