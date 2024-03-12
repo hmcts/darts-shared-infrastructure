ec1fbacc7c22c83887967ee5575c227c2ca21e9c
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-03-12 15:49:15 +0000

    Update migration-backup-policy.tf

--- migration-backup-policy.tf
+++ migration-backup-policy.tf
@@ -54,19 +54,3 @@ resource "azurerm_backup_protected_vm" "mig2" {
   source_vm_id        = azurerm_linux_virtual_machine.migration-linux2[each.key].id
   backup_policy_id    = azurerm_backup_policy_vm.darts-migration-backup.id
 }
-
-resource "azurerm_backup_protected_vm" "modernisationVMs" {
-  for_each            = var.modernisation_vms
-  resource_group_name = azurerm_resource_group.darts_resource_group.name
-  recovery_vault_name = azurerm_recovery_services_vault.darts-migration-backup.name
-  source_vm_id        = azurerm_windows_virtual_machine.modernisation_windows[each.key].id
-  backup_policy_id    = azurerm_backup_policy_vm.darts-migration-backup.id
-}
-
-resource "azurerm_backup_protected_vm" "modernisationLinux" {
-  for_each            = var.modernisation_linux_vms
-  resource_group_name = azurerm_resource_group.darts_resource_group.name
-  recovery_vault_name = azurerm_recovery_services_vault.darts-migration-backup.name
-  source_vm_id        = azurerm_linux_virtual_machine.modernisation-linux[each.key].id
-  backup_policy_id    = azurerm_backup_policy_vm.darts-migration-backup.id
-}
