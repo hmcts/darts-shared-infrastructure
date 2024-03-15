cfa9148a0d7fe34654898b21970965d2a1033384
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-03-15 17:02:20 +0000

    Update migration-backup-policy.tf

--- migration-backup-policy.tf
+++ migration-backup-policy.tf
@@ -28,7 +28,7 @@ resource "azurerm_backup_policy_vm" "darts-migration-backup" {
 resource "azurerm_backup_protected_vm" "vm" {
   for_each            = var.migration_vms
   resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
-  recovery_vault_name = azurerm_recovery_services_vault.darts-migration-backup.name
+  recovery_vault_name = azurerm_recovery_services_vault.darts-migration-backup[0].name
   source_vm_id        = azurerm_windows_virtual_machine.migration_windows[each.key].id
   backup_policy_id    = azurerm_backup_policy_vm.darts-migration-backup.id
 }
@@ -36,7 +36,7 @@ resource "azurerm_backup_protected_vm" "vm" {
 resource "azurerm_backup_protected_vm" "oracle" {
   for_each            = var.oracle_linux_vms
   resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
-  recovery_vault_name = azurerm_recovery_services_vault.darts-migration-backup.name
+  recovery_vault_name = azurerm_recovery_services_vault.darts-migration-backup[0].name
   source_vm_id        = azurerm_linux_virtual_machine.oracle[each.key].id
   backup_policy_id    = azurerm_backup_policy_vm.darts-migration-backup.id
 }
@@ -44,7 +44,7 @@ resource "azurerm_backup_protected_vm" "oracle" {
 resource "azurerm_backup_protected_vm" "mig1" {
   for_each            = var.migration_linux_vms
   resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
-  recovery_vault_name = azurerm_recovery_services_vault.darts-migration-backup.name
+  recovery_vault_name = azurerm_recovery_services_vault.darts-migration-backup[0].name
   source_vm_id        = azurerm_linux_virtual_machine.migration-linux[each.key].id
   backup_policy_id    = azurerm_backup_policy_vm.darts-migration-backup.id
 }
@@ -52,7 +52,7 @@ resource "azurerm_backup_protected_vm" "mig1" {
 resource "azurerm_backup_protected_vm" "mig2" {
   for_each            = var.migration_linux_vms2
   resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
-  recovery_vault_name = azurerm_recovery_services_vault.darts-migration-backup.name
+  recovery_vault_name = azurerm_recovery_services_vault.darts-migration-backup[0].name
   source_vm_id        = azurerm_linux_virtual_machine.migration-linux2[each.key].id
   backup_policy_id    = azurerm_backup_policy_vm.darts-migration-backup.id
 }
