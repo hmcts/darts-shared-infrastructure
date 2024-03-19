dc52ad5eb0c835048e9391de685eb719aa3e5ada
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-03-19 11:47:05 +0000

    DDM-430-Log-analytics-workspace (#188)
    * Create log-analytics.tf
    
    * Update log-analytics.tf
    
    Co-authored-by: Alex Bance <abance@bancey.xyz>
    Signed-off-by: scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
    
    * reverting sku
    
    ---------
    
    Signed-off-by: scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
    Co-authored-by: Alex Bance <abance@bancey.xyz>

---
+++ log-analytics.tf
@@ -0,0 +1,8 @@
+resource "azurerm_log_analytics_workspace" "migration-analytics" {
+  count               = local.is_migration_environment ? 1 : 0
+  name                = "darts-migration-analytics-workspace"
+  location            = azurerm_resource_group.darts_migration_resource_group[0].location
+  resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
