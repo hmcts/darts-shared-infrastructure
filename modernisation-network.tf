fe979a0aba5cd8f518b21141c80ca9e5ab33f418
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-03-25 15:20:18 +0000

    adding indexes

--- modernisation-network.tf
+++ modernisation-network.tf
@@ -42,7 +42,7 @@ resource "azurerm_virtual_network_peering" "darts_modernisation_to_hub" {
   name                         = "darts-modernisation-to-hub-${var.env}"
   resource_group_name          = azurerm_resource_group.darts_resource_group.name
   virtual_network_name         = azurerm_virtual_network.modernisation[0].name
-  remote_virtual_network_id    = data.azurerm_virtual_network.hub-south-vnet.id
+  remote_virtual_network_id    = data.azurerm_virtual_network.hub-south-vnet[0].id
   allow_virtual_network_access = true
   allow_forwarded_traffic      = true
   allow_gateway_transit        = false
--- modernisation-vms.tf
+++ modernisation-vms.tf
@@ -68,7 +68,7 @@ resource "azurerm_network_interface" "modernisation_vms_test" {
 
   ip_configuration {
     name                          = "migration-ipconfig"
-    subnet_id                     = azurerm_subnet.modernisation.id
+    subnet_id                     = azurerm_subnet.modernisation[0].id
     private_ip_address_allocation = "Static"
     private_ip_address            = each.value.ip_address
   }
