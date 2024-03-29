77f5f750e6172c0dbda0b258d47b055d536667a9
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-03-29 09:23:40 +0000

    Feat-event-grid (#204)
    * creating the event grid for the logic app
    
    * removing event grid file
    
    * Delete event-grid.tf

--- event-grid.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_eventgrid_topic" "av-scanner" {
-  name                = "av-scanner-output"
-  resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
-  location            = azurerm_resource_group.darts_migration_resource_group[0].location
-}
-
-
