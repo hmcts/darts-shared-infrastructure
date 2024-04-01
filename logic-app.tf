ba9632c5b90ab65e540c39beb23a404b1e15b78d
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-04-01 15:03:05 +0100

    Update logic-app.tf

--- logic-app.tf
+++ logic-app.tf
@@ -1,83 +1,71 @@
-# resource "azurerm_logic_app_workflow" "example" {
-#   name                = "example-logic-app"
-#   location            = azurerm_resource_group.darts_migration_resource_group[0].location
-#   resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
+resource "azurerm_logic_app_workflow" "example" {
+  name                = "example-logic-app"
+  location            = azurerm_resource_group.darts_migration_resource_group[0].location
+  resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
 
-#   definition = 
-# {
-#     "$schema": "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#",
-#     "contentVersion": "1.0.0.0",
-#     "triggers": {
-#         "manual": {
-#             "type": "Request",
-#             "kind": "Http",
-#             "inputs": {
-#                 "schema": {}
-#             }
-#         }
-#     },
-#     "actions": {
-#         "Initialize_variable": {
-#             "inputs": {
-#                 "variables": [
-#                     {
-#                         "name": "eventData",
-#                         "type": "Object",
-#                         "value": "@triggerBody()"
-#                     }
-#                 ]
-#             },
-#             "runAfter": {},
-#             "type": "InitializeVariable"
-#         },
-#         "Respond_to_HTTP_request": {
-#             "inputs": {
-#                 "body": {
-#                     "message": "Hello from Logic App triggered by Event Grid!"
-#                 },
-#                 "headers": {
-#                     "Content-Type": "application/json"
-#                 },
-#                 "statusCode": 200
-#             },
-#             "runAfter": {
-#                 "Initialize_variable": [
-#                     "Succeeded"
-#                 ]
-#             },
-#             "type": "Response"
-#         }
-#     },
-#     "outputs": {}
-# }
-# EOF
-# }
+  workflow_schema = <<EOF
+{
+    "$schema": "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#",
+    "contentVersion": "1.0.0.0",
+    "triggers": {
+        "manual": {
+            "type": "Request",
+            "kind": "Http",
+            "inputs": {
+                "schema": {}
+            }
+        }
+    },
+    "actions": {
+        "Initialize_variable": {
+            "inputs": {
+                "variables": [
+                    {
+                        "name": "eventData",
+                        "type": "Object",
+                        "value": "@triggerBody()"
+                    }
+                ]
+            },
+            "runAfter": {},
+            "type": "InitializeVariable"
+        },
+        "Execute_PowerShell_Script": {
+            "inputs": {
+                "body": {
+                    "script": "Your PowerShell script here"
+                },
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "method": "POST",
+                "uri": "<URL of your PowerShell script>"
+            },
+            "runAfter": {
+                "Initialize_variable": [
+                    "Succeeded"
+                ]
+            },
+            "type": "Http"
+        }
+    },
+    "outputs": {}
+}
+EOF
+}
 
-# resource "azurerm_logic_app_trigger_eventgrid" "av-scan-trigger" {
-#   name               = "example-eventgrid-trigger"
-#   logic_app_id       = azurerm_logic_app_workflow.example.id
-#   resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
+resource "azurerm_eventgrid_event_subscription" "example" {
+  name                  = "example-event-subscription"
+  scope                 = azurerm_resource_group.example.id
+  event_delivery_schema = "EventGridSchema"
+  
+  webhook_endpoint {
+    url = azurerm_logic_app_workflow.example.workflow_endpoint
+  }
 
-#   event_subscription {
-#     name                 = "av-scan-subscription"
-#     scope                = azurerm_eventgrid_topic.example.id
-#     subject_filter {
-#       subject_begins_with = ""
-#     }
-#     included_event_types = ["All"]
-#     webhook_endpoint {
-#       url = "https://<your-webhook-endpoint>"
-#     }
-#   }
-# }
+  storage_queue_endpoint {
+    storage_account_id = module.sa-migration-standard[0].id
+    queue_name         = azurerm_storage_queue.example.name
+  }
+}
 
-
-# resource "azurerm_eventgrid_event_subscription" "av-scanner-subscripton" {
-#   name                      = "av-scan-subscription"
-#   scope                     = azurerm_eventgrid_topic.example.id
-#   event_delivery_schema    = "EventGridSchema"
-#   included_event_types     = ["All"]
-#   webhook_endpoint {
-#     url = "https://your-webhook-endpoint.com"
-#   }
-# }
