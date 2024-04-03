c75d1d895949f097a27f2218f009fd55b2030d56
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-04-03 16:12:05 +0100

    Feat-av-event-grid-name (#212)
    * Create event-grid.tf
    
    * Update event-grid.tf
    
    * Update event-grid.tf
    
    * Update event-grid.tf
    
    ---------
    
    Signed-off-by: scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>

--- event-grid.tf
+++ event-grid.tf
@@ -1,6 +1,6 @@
 resource "azurerm_eventgrid_topic" "mig-scanner" {
   count               = local.is_migration_environment ? 1 : 0
-  name                = "mig-scanner-event"
+  name                = "${var.product}-event-${var.env}"
   resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
   location            = azurerm_resource_group.darts_migration_resource_group[0].location
 }
