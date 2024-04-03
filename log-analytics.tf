0e8fc4f28436a5dcba2c465a30468f2ca59ac5fa
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-04-03 12:11:39 +0100

    Update log-analytics.tf
    Signed-off-by: Enda Kelly <80516065+endakelly@users.noreply.github.com>

--- log-analytics.tf
+++ log-analytics.tf
@@ -1,6 +1,6 @@
 resource "azurerm_log_analytics_workspace" "migration-analytics" {
   count               = local.is_migration_environment ? 1 : 0
-  name                = "darts-migration-analytics-workspace"
+  name                = "darts-migration-analytics-workspace2"
   location            = azurerm_resource_group.darts_migration_resource_group[0].location
   resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
   sku                 = "PerGB2018"
