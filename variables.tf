7255de0cf35174da9bbdfaf9060b449d1883b3ee
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-04-25 09:57:02 +0100

    feat-docker-vm-no-aad (#221)
    * docker
    
    * Update prod.tfvars
    
    * Update prod.tfvars
    
    * Update migration-vms.tf
    
    * added subnet
    
    * Update prod.tfvars
    
    * moving docker vm from aad extension
    
    * Update migration-vms.tf
    
    * Update migration-vms.tf
    
    ---------
    
    Signed-off-by: scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -205,6 +205,15 @@ variable "migration_linux_vms" {
   default     = {}
 }
 
+variable "migration_docker_vms" {
+  type = map(object({
+    ip_address = string
+    subnet         = optional(string, "migration-subnet")
+  }))
+  description = "Map of objects describing the migration linux docker virtual machines to create."
+  default     = {}
+}
+
 variable "modernisation_linux_vms" {
   type = map(object({
     ip_address = string
