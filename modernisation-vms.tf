027de936e4839129fd011da4f20ac80f8033a02b
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-05-16 09:52:06 +0100

    Feat-alerts-test-env (#236)
    * Update alerts.tf
    
    * Update alerts.tf
    
    * Update modernisation-vms.tf

--- modernisation-vms.tf
+++ modernisation-vms.tf
@@ -133,7 +133,7 @@ resource "azurerm_network_interface" "modernisation-linux-nic" {
 
   ip_configuration {
     name                          = "migration-ipconfig"
-    subnet_id                     = azurerm_subnet.modernisation[0].id
+    subnet_id                     = var.vm_subnet_id
     private_ip_address_allocation = "Static"
     private_ip_address            = each.value.ip_address
   }
