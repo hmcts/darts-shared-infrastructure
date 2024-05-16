332d333810f412d92cad825b48e3e700374a8551
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-05-16 15:43:32 +0100

    Update alerts.tf

--- alerts.tf
+++ alerts.tf
@@ -38,3 +38,50 @@ resource "azurerm_monitor_action_group" "blob-action-group" {
     use_common_alert_schema = true
   }
 }
+
+resource "azurerm_monitor_metric_alert" "partition_capacity_alert" {
+  name                = "partition-capacity-alert"
+  resource_group_name = "<resource-group-name>"
+  scopes              = ["/subscriptions/<subscription-id>/resourceGroups/<resource-group-name>/providers/Microsoft.Compute/virtualMachines/<vm-name>"]
+  description         = "Alert triggered when a partition on the Windows VM reaches 80% capacity"
+  enabled             = true
+
+  criteria {
+    metric_namespace  = "Microsoft.Compute/virtualMachines"
+    metric_name       = "LogicalDisk % Free Space"
+    aggregation       = "Average"
+    operator          = "LessThanOrEqual"
+    threshold         = 20  # 100 - 80 (80% capacity)
+    dimension {
+      name     = "ResourceId"
+      operator = "Include"
+      values   = ["<vm-id>"]
+    }
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_monitor_action_group" "example" {
+  name                = "example-action-group"
+  resource_group_name = "<resource-group-name>"
+  short_name          = "example"
+  
+  email_receiver {
+    name                    = "email1"
+    email_address           = "user1@example.com"
+    use_common_alert_schema = true
+  }
+  
+  email_receiver {
+    name                    = "email2"
+    email_address           = "user2@example.com"
+    use_common_alert_schema = true
+  }
+}
