88dd4c59d91a73d34532c72a0f9ce334e621fb30
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-05-22 08:38:03 +0100

    feat-partition-capacity-alert (#238)
    * Update alerts.tf
    
    * Update alerts.tf
    
    * Update modernisation-vms.tf
    
    * fmt, and partition capacity alert
    
    * Update alerts.tf
    
    * Update alerts.tf
    
    * Update variables.tf
    
    * Update prod.tfvars
    
    * partition alert
    
    * Update alerts.tf
    
    * Update alerts.tf
    
    ---------
    
    Signed-off-by: scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>

--- data-factory.tf
+++ data-factory.tf
@@ -3,11 +3,11 @@ module "datafactory" {
   count = local.is_migration_environment ? 1 : 0
 
   source = "git@github.com:hmcts/terraform-module-azure-datafactory?ref=main"
-  
-  env    = var.env
-  product   = var.product
-  component = var.component
-  location = var.location
+
+  env         = var.env
+  product     = var.product
+  component   = var.component
+  location    = var.location
   common_tags = var.common_tags
 
   existing_resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
--- modernisation-network.tf
+++ modernisation-network.tf
@@ -4,8 +4,8 @@ resource "azurerm_virtual_network" "modernisation" {
   address_space       = concat(local.vnet_address_space, local.palo_address_space)
   location            = azurerm_resource_group.darts_resource_group.location
   resource_group_name = azurerm_resource_group.darts_resource_group.name
-  dns_servers = ["10.128.0.4", "10.128.0.5"]
-  tags        = var.common_tags
+  dns_servers         = ["10.128.0.4", "10.128.0.5"]
+  tags                = var.common_tags
 }
 
 resource "azurerm_subnet" "modernisation" {
