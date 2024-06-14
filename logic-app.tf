4e0f984bcb67a908cbdd29e7b2b1a9301c623422
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-06-14 10:40:47 +0100

    remove event grid

--- logic-app.tf
+++ logic-app.tf
@@ -1,66 +1,9 @@
 resource "azurerm_logic_app_workflow" "migration_scan" {
-  name                = "example-logic-app"
+  name                = "migration-logic-app"
   location            = azurerm_resource_group.darts_migration_resource_group[0].location
   resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
 
-   template_content = <<TEMPLATE
-{
-    "$schema": "https://schema.management.azure.com/providers/Microsoft.Logic/schemas/2016-06-01/workflowdefinition.json#",
-    "contentVersion": "1.0.0.0",
-    "triggers": {
-        "manual": {
-            "type": "Request",
-            "kind": "Http",
-            "inputs": {
-                "schema": {}
-            }
-        }
-    },
-    "actions": {
-        "Initialize_variable": {
-            "inputs": {
-                "variables": [
-                    {
-                        "name": "eventData",
-                        "type": "Object",
-                        "value": "@triggerBody()"
-                    }
-                ]
-            },
-            "runAfter": {},
-            "type": "InitializeVariable"
-        },
-        "Execute_PowerShell_Script": {
-            "inputs": {
-                "body": {
-                    "script": "Your PowerShell script here"
-                },
-                "headers": {
-                    "Content-Type": "application/json"
-                },
-                "method": "POST",
-                "uri": "<URL of your PowerShell script>"
-            },
-            "runAfter": {
-                "Initialize_variable": [
-                    "Succeeded"
-                ]
-            },
-            "type": "Http"
-        }
-    },
-    "outputs": {}
-}
-TEMPLATE
-depends_on = [
-  azurerm_logic_app_workflow.example
-]
 }
 
-resource "azurerm_eventgrid_event_subscription" "mig-event-supscription" {
-  name                  = "${var.product}-event-${var.env}-subscription"
-  scope                 =  azurerm_resource_group.darts_migration_resource_group[0].id
-  event_delivery_schema = "EventGridSchema"
-  
-}
+
 
