4a682f269f014e3468182f1fb17543b0cf14e243
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-07-12 16:54:48 +0100

    Update migration-blob-storage.tf

--- migration-blob-storage.tf
+++ migration-blob-storage.tf
@@ -62,38 +62,11 @@ module "sa-dets-standard" {
   common_tags                                = var.common_tags
 }
 
-resource "azurerm_storage_blob" "dets-st" {
-  count                  = local.is_test_environment ? 1 : 0
-  name                   = "${var.product}-dets-blob-st-${var.env}"
-  storage_account_name   = module.sa-dets-standard[0].storageaccount_name
-  storage_container_name = local.darts_migration_container
-  type                   = "Block"
-}
-
-module "sa-dets-standard2" {
-  source                                     = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  env                                        = var.env
-  storage_account_name                       = "${var.env}dets${var.product}2"
-  resource_group_name                        = azurerm_resource_group.darts_resource_group.name
-  location                                   = var.location
-  account_kind                               = "StorageV2"
-  account_tier                               = "Standard"
-  account_replication_type                   = "ZRS"
-  containers                                 = local.containers-mig
-  private_endpoint_subnet_id                 = data.azurerm_subnet.private_endpoints_dets.id
-  enable_nfs                                 = true
-  enable_hns                                 = true
-  enable_data_protection                     = true
-  enable_versioning                          = false
-  defender_enabled                           = var.defender_enable
-  defender_malware_scanning_enabled          = var.defender_scan
-  defender_malware_scanning_cap_gb_per_month = 250000
-  common_tags                                = var.common_tags
-}
-
-# resource "azurerm_storage_blob" "dets-st2" {
+# resource "azurerm_storage_blob" "dets-st" {
+#   count                  = local.is_test_environment ? 1 : 0
 #   name                   = "${var.product}-dets-blob-st-${var.env}"
-#   storage_account_name   = module.sa-dets-standard.storageaccount_name
+#   storage_account_name   = module.sa-dets-standard[0].storageaccount_name
 #   storage_container_name = local.darts_migration_container
 #   type                   = "Block"
 # }
+
