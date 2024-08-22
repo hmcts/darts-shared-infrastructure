5b6293e974a147f6bc97ff5816634b338df89382
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-08-22 10:29:15 +0100

    Update networking.tf (#271)

--- networking.tf
+++ networking.tf
@@ -72,11 +72,12 @@ resource "azurerm_network_security_rule" "dets-to-bias" {
 }
 
 resource "azurerm_network_security_rule" "block_internet" {
-  count                       = local.is_migration_environment ? 1 : 0
-  name                        = "BlockInternet"
-  priority                    = 160
-  direction                   = "Outbound"
-  access                      = var.env == "prod" ? "Deny" : "Allow"
+  count     = local.is_migration_environment ? 1 : 0
+  name      = "BlockInternet"
+  priority  = 160
+  direction = "Outbound"
+  # access                      = var.env == "prod" ? "Deny" : "Allow"
+  access                      = "Allow"
   protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "*"
