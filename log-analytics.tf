b88fe32f08436b0a1f5347cc8683175e315ff3df
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-09-03 14:15:48 +0100

    Update log-analytics.tf

--- log-analytics.tf
+++ log-analytics.tf
@@ -7,3 +7,13 @@ resource "azurerm_log_analytics_workspace" "migration-analytics" {
   retention_in_days   = 30
   tags                = var.common_tags
 }
+
+resource "azurerm_log_analytics_workspace" "postgres-analytics" {
+  count               = local.is_migration_environment ? 1 : 0
+  name                = "darts-postgres-analytics-workspace"
+  location            = azurerm_resource_group.darts_migration_resource_group[0].location
+  resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = var.common_tags
+}
