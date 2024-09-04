d2d6e32dea10a89353a641d6bc8704486df9bfb1
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-09-04 14:53:17 +0100

    Update log-analytics.tf

--- log-analytics.tf
+++ log-analytics.tf
@@ -26,6 +26,24 @@ resource "azurerm_monitor_diagnostic_setting" "example" {
   enabled_log {
     category = "AuditEvent"
   }
+  enabled_log {
+    category = "PostgreSQLLogs"
+  }
+  enabled_log {
+    category = "PostgreSQLFlexSessions"
+  }
+  enabled_log {
+    category = "PostgreSQLFlexQueryStoreRuntime"
+  }
+  enabled_log {
+    category = "PostgreSQLFlexQueryStoreWaitStats"
+  }
+  enabled_log {
+    category = "PostgreSQLFlexTableStats"
+  }
+  enabled_log {
+    category = "PostgreSQLFlexDatabaseXacts"
+  }
   metric {
     category = "AllMetrics"
   }
