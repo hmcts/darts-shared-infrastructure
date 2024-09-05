4ef208fa02c3f84a1ead8d155096747e590c1a04
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-09-05 13:13:03 +0100

    Update database.tf

--- database.tf
+++ database.tf
@@ -162,7 +162,7 @@ moved {
 resource "azurerm_monitor_diagnostic_setting" "migration-diagnostic" {
   count                      = local.is_migration_environment ? 1 : 0
   name                       = "migration-postgres-diagnostics"
-  target_resource_id         = module.postgresql_flexible[0].id
+  target_resource_id         = module.postgresql_flexible.postgresql.id
   log_analytics_workspace_id = azurerm_log_analytics_workspace.postgres-analytics[0].id
 
   enabled_log {
