0461f48be9cd27d4a71b0e1663926796afdaae95
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2024-10-15 10:51:40 +0100

    Update storage-acc-dets.tf

--- storage-acc-dets.tf
+++ storage-acc-dets.tf
@@ -8,17 +8,12 @@ data "azurerm_subnet" "private_endpoints_dets_sa" {
 
 module "sa_dets" {
   source = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-
   env = var.env
-
   storage_account_name = local.dets_storage_account_name
   common_tags          = var.common_tags
-
   default_action = "Allow"
-
   resource_group_name = azurerm_resource_group.darts_migration_resource_group[0].name
   location            = var.location
-
   account_tier                    = var.sa_account_tier
   account_kind                    = var.sa_account_kind
   account_replication_type        = var.sa_account_replication_type
@@ -26,19 +21,14 @@ module "sa_dets" {
   allow_nested_items_to_be_public = "true"
   enable_change_feed              = true
   private_endpoint_subnet_id      = data.azurerm_subnet.private_endpoints_dets_sa.id
-
+  defender_enabled                           = var.defender_enable
+  defender_malware_scanning_enabled          = var.defender_scan
+  defender_malware_scanning_cap_gb_per_month = 90
   enable_data_protection = true
-
-  containers = local.containers
-
-  cors_rules = [{
-    allowed_headers    = ["*"]
-    allowed_methods    = ["GET", "OPTIONS"]
-    allowed_origins    = ["https://hmctsdartsb2csbox.b2clogin.com"]
-    exposed_headers    = ["*"]
-    max_age_in_seconds = 200
-  }]
-
+  containers                                 = local.containers-mig
+  enable_nfs                                 = true
+  enable_sftp                                = var.enable_sftp
+  enable_versioning                          = false
 }
 
 resource "azurerm_storage_blob" "dets" {
