595b35a7c904596918f20d6314f25f5ce3f92408
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2024-10-16 10:43:53 +0000

    Updating Terraform Formatting

--- networking.tf
+++ networking.tf
@@ -72,10 +72,10 @@ resource "azurerm_network_security_rule" "dets-to-bias" {
 }
 
 resource "azurerm_network_security_rule" "block_internet" {
-  count                       = local.is_migration_environment ? 1 : 0
-  name                        = "BlockInternet"
-  priority                    = 160
-  direction                   = "Outbound"
+  count     = local.is_migration_environment ? 1 : 0
+  name      = "BlockInternet"
+  priority  = 160
+  direction = "Outbound"
   # access                      = var.env == "prod" ? "Deny" : "Allow"
   access                      = "Allow"
   protocol                    = "*"
