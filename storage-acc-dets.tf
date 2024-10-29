4f8321e0cc3de11fc3791c3027e5f66abd8c8322
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2024-10-29 09:28:55 +0000

    Updating Terraform Formatting

--- storage-acc-dets.tf
+++ storage-acc-dets.tf
@@ -7,26 +7,26 @@ data "azurerm_subnet" "private_endpoints_dets_sa" {
 }
 
 module "sa_dets" {
-  source                                     = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-  env                                        = var.env
-  storage_account_name                       = local.dets_storage_account_name
-  common_tags                                = var.common_tags
-  resource_group_name                        = azurerm_resource_group.darts_migration_resource_group[0].name
-  location                                   = var.location
-  account_tier                               = var.sa_account_tier
-  account_kind                               = var.sa_account_kind
-  account_replication_type                   = var.sa_account_replication_type
-  access_tier                                = var.sa_access_tier
-  allow_nested_items_to_be_public            = "true"
-  enable_change_feed                         = true
-  private_endpoint_subnet_id                 = data.azurerm_subnet.private_endpoints_dets_sa.id
-  defender_enabled                           = var.defender_enable
-  defender_malware_scanning_enabled          = var.defender_scan
-  enable_data_protection                     = true
-  containers                                 = local.containers-dets
-  enable_versioning                          = false
-  enable_nfs                                 = true
-  enable_sftp                                = var.enable_sftp
+  source                            = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
+  env                               = var.env
+  storage_account_name              = local.dets_storage_account_name
+  common_tags                       = var.common_tags
+  resource_group_name               = azurerm_resource_group.darts_migration_resource_group[0].name
+  location                          = var.location
+  account_tier                      = var.sa_account_tier
+  account_kind                      = var.sa_account_kind
+  account_replication_type          = var.sa_account_replication_type
+  access_tier                       = var.sa_access_tier
+  allow_nested_items_to_be_public   = "true"
+  enable_change_feed                = true
+  private_endpoint_subnet_id        = data.azurerm_subnet.private_endpoints_dets_sa.id
+  defender_enabled                  = var.defender_enable
+  defender_malware_scanning_enabled = var.defender_scan
+  enable_data_protection            = true
+  containers                        = local.containers-dets
+  enable_versioning                 = false
+  enable_nfs                        = true
+  enable_sftp                       = var.enable_sftp
 }
 
 # resource "azurerm_storage_blob" "dets" {
