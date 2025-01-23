364d159bf8cf100309f4234faf5b9c1a60de8981
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2025-01-23 15:42:29 +0000

    Update migration-vms.tf

--- migration-vms.tf
+++ migration-vms.tf
@@ -586,7 +586,7 @@ resource "azurerm_managed_disk" "shared_disk" {
 resource "azurerm_virtual_machine_data_disk_attachment" "shared_disk_attachment" {
   for_each          = local.target_vms_for_shared_disk
   managed_disk_id   = azurerm_managed_disk.shared_disk.id
-  virtual_machine_id = azurerm_virtual_machine.migration_vms[each.key].id
+  virtual_machine_id = azurerm_virtual_machine.migration_windows[each.key].id
   lun               = 0
   caching           = "None"
 }
