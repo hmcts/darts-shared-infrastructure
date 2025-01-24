7b938ed88de4b82cbf2b30fe6c0aed96ea5b0d41
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2025-01-24 10:59:58 +0000

    Updating Terraform Formatting

--- migration-vms.tf
+++ migration-vms.tf
@@ -618,9 +618,9 @@ resource "azurerm_managed_disk" "shared_disk" {
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "shared_disk_attachment" {
-  for_each            = toset(["prddartsmig01", "prddartsassess", "prddartsassure", "prddartsoracle", "prddartsunstr"])
-  managed_disk_id     = azurerm_managed_disk.shared_disk.id
-  virtual_machine_id  = azurerm_windows_virtual_machine.migration_windows[each.key].id
-  lun                 = 0 # Logical Unit Number
-  caching             = "None"
+  for_each           = toset(["prddartsmig01", "prddartsassess", "prddartsassure", "prddartsoracle", "prddartsunstr"])
+  managed_disk_id    = azurerm_managed_disk.shared_disk.id
+  virtual_machine_id = azurerm_windows_virtual_machine.migration_windows[each.key].id
+  lun                = 0 # Logical Unit Number
+  caching            = "None"
 }
