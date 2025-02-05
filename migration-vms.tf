534790bba31566c5dd89722d80efa26fef944b70
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2025-02-05 09:29:09 +0000

    Update migration-vms.tf

--- migration-vms.tf
+++ migration-vms.tf
@@ -579,36 +579,36 @@ resource "azurerm_virtual_machine_data_disk_attachment" "shared_disk_attachment"
   caching            = "None"
 }
 
-locals {
-  mig-01-vm = {
-    for name, details in var.migration_vms :
-    name => details if contains(["prddartsmig01"], name)
-  }
-}
-resource "azurerm_managed_disk" "mig-01-disk" {
-  name                 = "migration-files-datadisk"
-  location             = azurerm_resource_group.darts_migration_resource_group[0].location
-  resource_group_name  = azurerm_resource_group.darts_migration_resource_group[0].name
-  storage_account_type = "Premium_LRS" # Required for shared disks
-  create_option        = "Empty"
-  disk_size_gb         = "8000"
-  max_shares           = 2 # Allows sharing with 2 VMs
-  tags                 = var.common_tags
-}
-# Attach the Disk to the Linux VM (RHEL)
-resource "azurerm_virtual_machine_data_disk_attachment" "linux_disk_attach" {
-  for_each           = var.oracle_linux_vms
-  managed_disk_id    = azurerm_managed_disk.mig-01-disk.id
-  virtual_machine_id = azurerm_linux_virtual_machine.oracle[each.key].id
-  lun                = 1
-  caching            = "None"
-}
+# locals {
+#   mig-01-vm = {
+#     for name, details in var.migration_vms :
+#     name => details if contains(["prddartsmig01"], name)
+#   }
+# }
+# resource "azurerm_managed_disk" "mig-01-disk" {
+#   name                 = "migration-files-datadisk"
+#   location             = azurerm_resource_group.darts_migration_resource_group[0].location
+#   resource_group_name  = azurerm_resource_group.darts_migration_resource_group[0].name
+#   storage_account_type = "Premium_LRS" # Required for shared disks
+#   create_option        = "Empty"
+#   disk_size_gb         = "8000"
+#   max_shares           = 2 # Allows sharing with 2 VMs
+#   tags                 = var.common_tags
+# }
+# # Attach the Disk to the Linux VM (RHEL)
+# resource "azurerm_virtual_machine_data_disk_attachment" "linux_disk_attach" {
+#   for_each           = var.oracle_linux_vms
+#   managed_disk_id    = azurerm_managed_disk.mig-01-disk.id
+#   virtual_machine_id = azurerm_linux_virtual_machine.oracle[each.key].id
+#   lun                = 1
+#   caching            = "None"
+# }
 
-# Attach the Disk to the Windows VM (Read-Only)
-resource "azurerm_virtual_machine_data_disk_attachment" "windows_disk_attach" {
-  for_each           = local.mig-01-vm
-  managed_disk_id    = azurerm_managed_disk.mig-01-disk.id
-  virtual_machine_id = azurerm_windows_virtual_machine.migration_windows[each.key].id
-  lun                = 2
-  caching            = "None"
-}
+# # Attach the Disk to the Windows VM (Read-Only)
+# resource "azurerm_virtual_machine_data_disk_attachment" "windows_disk_attach" {
+#   for_each           = local.mig-01-vm
+#   managed_disk_id    = azurerm_managed_disk.mig-01-disk.id
+#   virtual_machine_id = azurerm_windows_virtual_machine.migration_windows[each.key].id
+#   lun                = 2
+#   caching            = "None"
+# }
