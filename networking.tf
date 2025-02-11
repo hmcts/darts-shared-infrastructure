83b957a066a12f35e76d8f8307aae1c845d15674
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2025-02-11 10:53:59 +0000

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -50,7 +50,7 @@ resource "azurerm_network_security_rule" "allow_outbound_prddartsmigdock" {
   protocol                    = "*"
   source_port_range           = "*"
   destination_port_range      = "*"
-  source_address_prefix       = "10.24.239.167"
+  source_address_prefix       = "10.24.239.12"
   destination_address_prefix  = "*"
   network_security_group_name = azurerm_network_security_group.migration[0].name
   resource_group_name         = azurerm_resource_group.darts_migration_resource_group[0].name
