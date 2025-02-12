cac43275a1b520f65554ae871ac65c97fe26e5f8
scott-robertson1 <127202807+scott-robertson1@users.noreply.github.com>
2025-02-12 11:31:37 +0000

    Update networking.tf (#353)

--- networking.tf
+++ networking.tf
@@ -42,6 +42,21 @@ resource "azurerm_subnet_network_security_group_association" "migration" {
 }
 
 
+resource "azurerm_network_security_rule" "allow_outbound_postgress" {
+  count                       = local.is_migration_environment ? 1 : 0
+  name                        = "allow-outbound-assess-postgress"
+  priority                    = 140
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "10.24.239.5"
+  destination_address_prefix  = "10.144.40.14"
+  network_security_group_name = azurerm_network_security_group.migration[0].name
+  resource_group_name         = azurerm_resource_group.darts_migration_resource_group[0].name
+}
+
 resource "azurerm_network_security_rule" "allow_outbound_prddartsunstr" {
   count                       = local.is_migration_environment ? 1 : 0
   name                        = "allow-outbound-prddartsoracle"
