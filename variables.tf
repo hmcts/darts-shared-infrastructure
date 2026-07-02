d519df5d0051287b38096cdd5dab110651065bdf
Neil Perry <neil.perry@cgi.com>
2026-07-02 10:54:21 +0100

    update sub id

--- variables.tf
+++ variables.tf
@@ -123,6 +123,10 @@ variable "logic_apps_address_space" {
 
 variable "aks_subscription_id" {}
 
+variable "private_dns_subscription_id" {
+  default = "1baf5470-1c3e-40d3-a6f7-74bfbce4b348"
+}
+
 variable "firewall_route_ranges" {
   type        = list(string)
   description = "List of address ranges to route to the DARTS specific migration firewall."
