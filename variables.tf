8f4081049a0eb7278f0ea85cf0d576cca293a0c9
Neil Perry <neil.perry@cgi.com>
2026-08-13 11:38:19 +0100

    fix typo

--- variables.tf
+++ variables.tf
@@ -379,7 +379,7 @@ variable "daily_data_cap_in_gb" {
   default     = 50
 }
 
-variaable "migration_retention_period" {
+variable "migration_retention_period" {
   description = "The retention period for the migration storage account in days"
   default     = 14
 }
